f48c486ea6c3780792c414f8c384ea3a8725caa0
Mark Bayazit <mnbayazit@gmail.com>
2013-12-30 21:47:04 -0800

    added support for "name" option (fixes #1)

--- tasks/nunjucks.js
+++ tasks/nunjucks.js
@@ -8,27 +8,33 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     var nunjucks = require('nunjucks');
+    var lib = require('nunjucks/src/lib');
 
-    grunt.registerMultiTask('nunjucks', 'Precompile nunjucks', function() {
+    grunt.registerMultiTask('nunjucks', 'Precompile nunjucks', function () {
         // Merge task-specific and/or target-specific options with these defaults.
         var opts = this.options({
             asFunction: false,
             env: null
         });
 
-        var files = this.files.forEach(function(f) {
-            var src = f.src.filter(function(filepath) {
+        var nameFunc = lib.isFunction(opts.name) ? opts.name : function(filepath) {
+            return filepath.split('\\').join('/');
+        };
+
+        this.files.forEach(function (f) {
+            var src = f.src.filter(function (filepath) {
                 if (!grunt.file.exists(filepath)) {
                     grunt.log.warn('Source file "' + filepath + '" not found.');
                     return false;
                 } else {
                     return true;
                 }
-            }).map(function(filepath) {
-                return nunjucks.precompile(filepath, opts);
-            }).join('');
+            }).map(function (filepath) {
+                    opts.name = nameFunc(filepath);
+                    return nunjucks.precompile(filepath, opts);
+                }).join('');
 
             grunt.file.write(f.dest, src);
             grunt.log.writeln('File "' + f.dest + '" created.');
